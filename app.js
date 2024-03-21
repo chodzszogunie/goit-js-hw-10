2786a80f3241ddd41d15542bf5bb05db2d2a2256
chodzszogunie <151750408+chodzszogunie@users.noreply.github.com>
2024-03-21 10:30:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ import { countries } from './fetchCountries.js';
 const searchBox = document.getElementById('search-box');
 const countryInfo = document.getElementById('country-info');
 
-function displayCountries(countries) {
+const displayCountries = (countries) => {
   countryInfo.innerHTML = '';
   if (countries.length === 0) {
     return;
   } else if (countries.length > 10) {
-    alert('Znaleziono zbyt wiele pasujących wyników. Proszę wprowadź bardziej specyficzną nazwę.');
+    Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
   } else if (countries.length === 1) {
     const country = countries[0];
     const languages = country.languages.map(lang => lang.name).join(', ');
     countryInfo.innerHTML = `
       <div class="country-card">
-        <img src="${country.flags.svg}" alt="Flaga">
+        <img src="${country.flags.svg}" alt="Flag">
         <h2>${country.name.official}</h2>
-        <p><strong>Stolica:</strong> ${country.capital}</p>
-        <p><strong>Populacja:</strong> ${country.population}</p>
-        <p><strong>Języki:</strong> ${languages}</p>
+        <p><strong>Capital:</strong> ${country.capital}</p>
+        <p><strong>Population:</strong> ${country.population}</p>
+        <p><strong>Languages:</strong> ${languages}</p>
       </div>
     `;
   } else {
@@ -32,14 +32,14 @@ function displayCountries(countries) {
     });
     countryInfo.appendChild(countryList);
   }
-}
+};
 
 const fetchCountriesDebounced = _.debounce(async (name) => {
   try {
     const countries = await fetchCountries(name.trim());
     displayCountries(countries);
   } catch (error) {
-    alert(error.message);
+    Notiflix.Notify.failure(error.message);
   }
 }, 300);
 
@@ -51,3 +51,4 @@ searchBox.addEventListener('input', (event) => {
     fetchCountriesDebounced(searchTerm);
   }
 });
+
